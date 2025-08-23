d1e470d03ff24f790a612e4f569520647358dbdc
Navid <navid123@gmail.com>
2025-08-24 00:02:56 +0600

    feat: implement dummy profile and login

--- ezycommerceapp/app/src/main/java/com/example/ezycommerce_app/ProfileFragment.kt
+++ ezycommerceapp/app/src/main/java/com/example/ezycommerce_app/ProfileFragment.kt
@@ -1,15 +1,17 @@
 package com.example.ezycommerce_app
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class ProfileFragment : Fragment() {
 
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
 }
